8ebed19b49ba96e780dc423e1dabd9d0d9440688
LanBTH <lanbthhe160676@fpt.edu.vn>
2023-07-06 11:41:57 +0700

    feature: reset pasword screen

---
+++ app/src/main/java/com/example/finsmart/Activity/ResetPassWordActivity.java
@@ -0,0 +1,16 @@
+package com.example.finsmart.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.finsmart.R;
+
+public class ResetPassWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
