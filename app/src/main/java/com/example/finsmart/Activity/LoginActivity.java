cfa4d9f7b75cd7652f531db9de3b0c483c325b39
LanBTH <lanbthhe160676@fpt.edu.vn>
2023-07-13 19:48:37 +0700

    feature: login demo

--- app/src/main/java/com/example/finsmart/Activity/LoginActivity.java
+++ app/src/main/java/com/example/finsmart/Activity/LoginActivity.java
@@ -2,8 +2,12 @@ package com.example.finsmart.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +16,8 @@ import com.example.finsmart.R;
 public class LoginActivity extends AppCompatActivity {
 
     TextView forgetPassWord, signUp;
+    EditText edtEmail, edtPassword;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +34,36 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
             startActivity(intent);
         });
+
+        edtEmail = findViewById(R.id.edt_email_signin);
+        edtPassword = findViewById(R.id.edt_password_signin);
+        btnLogin = findViewById(R.id.btn_signin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login(){
+        String email = edtEmail.getText().toString().trim();
+        String pass = edtPassword.getText().toString().trim();
+        Log.d("Debug",email + pass);
+        if(email.isEmpty()){
+            edtEmail.setError("Please enter Email");
+        }
+        if(pass.isEmpty()){
+            edtPassword.setError("Please enter Password");
+        }
+        if (!email.equals("admin@gmail.com")  || !pass.equals("123")){
+            Toast.makeText(LoginActivity.this, "Please check Email/ Password", Toast.LENGTH_SHORT).show();
+        }
+
+        if (email.equals("admin@gmail.com") && pass.equals("123")){
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
