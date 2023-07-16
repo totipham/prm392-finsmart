ffd10fb200a4081a985b4baae2ae3ce2e626e979
David Pham <ducptm68@gmail.com>
2023-07-16 23:25:59 +0700

    fix: now show wallets

--- app/src/main/java/com/example/finsmart/Fragment/WalletFragment.java
+++ app/src/main/java/com/example/finsmart/Fragment/WalletFragment.java
@@ -2,22 +2,47 @@ package com.example.finsmart.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.finsmart.Activity.MainActivity;
+import com.example.finsmart.Adapter.EditWalletAdapter;
+import com.example.finsmart.Interface.RecyclerViewClickListener;
+import com.example.finsmart.Model.Wallet;
 import com.example.finsmart.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link WalletFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class WalletFragment extends Fragment {
+public class WalletFragment extends Fragment implements RecyclerViewClickListener {
+    FirebaseFirestore db;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    List<WalletWithCheck> walletList;
+    Wallet selectedWallet;
+
     public WalletFragment() {
         // Required empty public constructor
     }
@@ -31,14 +56,19 @@ public class WalletFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View dogshit = inflater.inflate(R.layout.fragment_wallet, container, false);
+        walletList = new ArrayList<>();
+        loadWalletList();
+        View view = inflater.inflate(R.layout.fragment_wallet, container, false);
         // Inflate the layout for this fragment
-        imageButton = (ImageButton) dogshit.findViewById(R.id.add_wallet_btn);
+        imageButton = (ImageButton) view.findViewById(R.id.add_wallet_btn);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,7 +77,62 @@ public class WalletFragment extends Fragment {
                         , "addWallet", "Add New Wallet");
             }
         });
-        return dogshit;
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void loadWalletList() {
+        db.collection("wallets")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            walletList.clear();
+                            boolean isFirst = true;
+
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                if (document.getString("belongTo").equals(mUser.getUid())) {
+                                    Wallet wallet = new Wallet(document.getId(), document.getString("name"), document.getDouble("balance"), document.getString("belongTo"));
+                                    walletList.add(new WalletWithCheck(wallet, isFirst));
+                                    isFirst = false;
+                                }
+                            }
+
+                            ((TextView) getView().findViewById(R.id.tv_my_wallet)).setText("My Wallets" + " (" + walletList.size() + ")");
+
+                            updateWalletRecyclerView(0);
+                        } else {
+                            Toast.makeText(getContext(), "Error getting wallets", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void updateWalletRecyclerView(int position) {
+        RecyclerView walletScrollList = (RecyclerView) getView().findViewById(R.id.rview_wallet_list);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        layoutManager.scrollToPosition(position);
+
+        EditWalletAdapter walletListAdapter = new EditWalletAdapter(walletList, this);
+        walletScrollList.setAdapter(walletListAdapter);
+        walletScrollList.setLayoutManager(layoutManager);
+
+//        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void recyclerViewListClicked(View v, int position) {
+        selectedWallet = walletList.get(position).getWallet();
+        for (int i = 0; i < walletList.size(); i++) {
+            walletList.get(i).setChecked(false);
+        }
+        walletList.get(position).setChecked(true);
+        updateWalletRecyclerView(position);
     }
 
 //    public void replaceFragment(Fragment fragment) {
