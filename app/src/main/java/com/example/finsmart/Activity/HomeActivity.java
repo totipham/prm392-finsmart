1f17fe6da67632898ef920f488546734fe81d8eb
Strongest <111754516+Strongweak@users.noreply.github.com>
2023-07-17 10:14:50 +0700

    feat: [FIN-30] implement loading transaction history from firebase

--- app/src/main/java/com/example/finsmart/Activity/HomeActivity.java
+++ app/src/main/java/com/example/finsmart/Activity/HomeActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.finsmart.Adapter.TransactionListAdapter;
 import com.example.finsmart.Adapter.WalletListAdapter;
@@ -26,6 +27,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,47 +39,80 @@ public class HomeActivity extends AppCompatActivity {
     private int dotscount;
     private ImageView[] dots;
 
+    private ArrayList<Transaction> transactionList;
+    FirebaseFirestore db;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Disable night mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
         setContentView(R.layout.activity_home);
 
-        generateWalletList();
-        generateTransactionList();
+        //generateWalletList();
+        //generateTransactionList();
+        GetTransactionData();
     }
 
     void generateTransactionList() {
-        List<Transaction> transactionList = new ArrayList<>();
+//        Transaction t1 = new Transaction("t1", "AI-Bank", TransactionType.DEPOSIT, false, "460.00", R.drawable.ticket_icon);
+//        Transaction t2 = new Transaction("t2", "McDonald", TransactionType.PAYMENT, true, "34.10", R.drawable.ticket_icon);
+//        Transaction t3 = new Transaction("t3", "Gym", TransactionType.DEPOSIT, false, "40.99", R.drawable.ticket_icon);
+//        Transaction t4 = new Transaction("t4", "AI-Bank", TransactionType.DEPOSIT, false, "460.00", R.drawable.ticket_icon);
+//
+//        transactionList.add(t1);
+//        transactionList.add(t2);
+//        transactionList.add(t3);
+//        transactionList.add(t4);
 
-        Transaction t1 = new Transaction("t1", "AI-Bank", TransactionType.DEPOSIT, false, "460.00", R.drawable.ticket_icon);
-        Transaction t2 = new Transaction("t2", "McDonald", TransactionType.PAYMENT, true, "34.10", R.drawable.ticket_icon);
-        Transaction t3 = new Transaction("t3", "Gym", TransactionType.DEPOSIT, false, "40.99", R.drawable.ticket_icon);
-        Transaction t4 = new Transaction("t4", "AI-Bank", TransactionType.DEPOSIT, false, "460.00", R.drawable.ticket_icon);
 
-        transactionList.add(t1);
-        transactionList.add(t2);
-        transactionList.add(t3);
-        transactionList.add(t4);
-
-        TransactionListAdapter transactionListAdapter = new TransactionListAdapter(transactionList);
-        RecyclerView transactionHistoryView = (RecyclerView) findViewById(R.id.rview_transaction_history);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        transactionHistoryView.setLayoutManager(layoutManager);
-        transactionHistoryView.setAdapter(transactionListAdapter);
     }
 
+    //nhờ ae copy paste vào các trang còn lại
+    void GetTransactionData(){
+        transactionList = new ArrayList<>();
+        db.collection("transactions")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                transactionList.clear();
+
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    if (document.getString("BelongTo").equals(mUser.getUid())) {
+                                        Transaction transaction = new Transaction(
+                                                document.getString("BelongTo"),
+                                                document.getDate("Date"),
+                                                document.getString("Name"),
+                                                TransactionType.DEPOSIT,
+                                                document.getBoolean("IsIncome"),
+                                                document.getString("Amount"),
+                                                R.drawable.ticket_icon
+                                                );
+                                        transactionList.add(transaction);
+                                    }
+                                }
+
+                                TransactionListAdapter transactionListAdapter = new TransactionListAdapter(transactionList);
+                                RecyclerView transactionHistoryView = (RecyclerView) findViewById(R.id.rview_transaction_history);
+                                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(HomeActivity.this);
+                                transactionHistoryView.setLayoutManager(layoutManager);
+                                transactionHistoryView.setAdapter(transactionListAdapter);
+                            } else {
+                                Toast.makeText(HomeActivity.this, "Error loading transaction", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+    }
     void generateWalletList() {
         List<Wallet> walletList = new ArrayList<>();
-
-//        Wallet w1 = new Wallet("w1", "Shopping", 1000000);
-//        Wallet w2 = new Wallet("w2", "Education", 1000000);
-//        Wallet w3 = new Wallet("w3", "Investment", 1000000);
-//
-//        walletList.add(w1);
-//        walletList.add(w2);
-//        walletList.add(w3);
 
         viewPager = (ViewPager2) findViewById(R.id.view_pager);
         sliderDotspanel = (LinearLayout) findViewById(R.id.slider_dots);
